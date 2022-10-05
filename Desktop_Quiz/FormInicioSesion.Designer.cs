12615ab5957356de256cd4579ff6f28c48a43b15
Pol Crespo <pcrespoh2122@politecnics.barcelona>
2022-10-05 13:21:47 +0200

    Form LogIn Image IconLogIn

--- Desktop_Quiz/FormInicioSesion.Designer.cs
+++ Desktop_Quiz/FormInicioSesion.Designer.cs
@@ -28,19 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelNom = new System.Windows.Forms.Label();
+            this.labelContrasenya = new System.Windows.Forms.Label();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.textBoxContrasenya = new System.Windows.Forms.TextBox();
+            this.groupBoxDades = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBoxDades.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::Desktop_Quiz.Properties.Resources.loginImage;
+            this.pictureBox1.Location = new System.Drawing.Point(815, 128);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(829, 727);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 86);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "INICIA SESSIÓ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // labelNom
+            // 
+            this.labelNom.AutoSize = true;
+            this.labelNom.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNom.Location = new System.Drawing.Point(174, 112);
+            this.labelNom.Name = "labelNom";
+            this.labelNom.Size = new System.Drawing.Size(39, 17);
+            this.labelNom.TabIndex = 0;
+            this.labelNom.Text = "Nom";
+            // 
+            // labelContrasenya
+            // 
+            this.labelContrasenya.AutoSize = true;
+            this.labelContrasenya.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelContrasenya.Location = new System.Drawing.Point(174, 188);
+            this.labelContrasenya.Name = "labelContrasenya";
+            this.labelContrasenya.Size = new System.Drawing.Size(91, 17);
+            this.labelContrasenya.TabIndex = 1;
+            this.labelContrasenya.Text = "Contrasenya";
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(342, 105);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(333, 20);
+            this.textBoxNom.TabIndex = 2;
+            // 
+            // textBoxContrasenya
+            // 
+            this.textBoxContrasenya.Location = new System.Drawing.Point(342, 180);
+            this.textBoxContrasenya.Name = "textBoxContrasenya";
+            this.textBoxContrasenya.Size = new System.Drawing.Size(333, 20);
+            this.textBoxContrasenya.TabIndex = 3;
+            // 
+            // groupBoxDades
+            // 
+            this.groupBoxDades.Controls.Add(this.textBoxContrasenya);
+            this.groupBoxDades.Controls.Add(this.textBoxNom);
+            this.groupBoxDades.Controls.Add(this.labelContrasenya);
+            this.groupBoxDades.Controls.Add(this.labelNom);
+            this.groupBoxDades.Location = new System.Drawing.Point(510, 350);
+            this.groupBoxDades.Name = "groupBoxDades";
+            this.groupBoxDades.Size = new System.Drawing.Size(845, 337);
+            this.groupBoxDades.TabIndex = 0;
+            this.groupBoxDades.TabStop = false;
             // 
             // FormInicioSesion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBoxDades);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormInicioSesion";
             this.Text = "FormInicioSesion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBoxDades.ResumeLayout(false);
+            this.groupBoxDades.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelNom;
+        private System.Windows.Forms.Label labelContrasenya;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.TextBox textBoxContrasenya;
+        private System.Windows.Forms.GroupBox groupBoxDades;
     }
 }
