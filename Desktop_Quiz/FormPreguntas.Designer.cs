a57d5f537b7fe265303398e34e2e7aa980c6f11b
MarioMolina27 <molinaballesteros@hotmail.com>
2022-10-06 13:22:49 +0200

    json cargados

--- Desktop_Quiz/FormPreguntas.Designer.cs
+++ Desktop_Quiz/FormPreguntas.Designer.cs
@@ -30,24 +30,24 @@
         {
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.groupBoxPreguntes = new System.Windows.Forms.GroupBox();
-            this.labelTitul = new System.Windows.Forms.Label();
-            this.textBoxTitulo = new System.Windows.Forms.TextBox();
-            this.buttonAcceptarTitul = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBoxOpcionsFilOr = new System.Windows.Forms.GroupBox();
-            this.labelOrdenacio = new System.Windows.Forms.Label();
-            this.groupBoxOrdenacio = new System.Windows.Forms.GroupBox();
-            this.radioButtonCategoria = new System.Windows.Forms.RadioButton();
-            this.radioButtonDificultat = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.buttonAfegir = new System.Windows.Forms.Button();
-            this.buttonEditar = new System.Windows.Forms.Button();
             this.buttonEliminar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonAfegir = new System.Windows.Forms.Button();
+            this.groupBoxOrdenacio = new System.Windows.Forms.GroupBox();
+            this.radioButtonDificultat = new System.Windows.Forms.RadioButton();
+            this.radioButtonCategoria = new System.Windows.Forms.RadioButton();
+            this.labelOrdenacio = new System.Windows.Forms.Label();
+            this.dataGridViewPelicules = new System.Windows.Forms.DataGridView();
+            this.buttonAcceptarTitul = new System.Windows.Forms.Button();
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            this.labelTitul = new System.Windows.Forms.Label();
             this.groupBoxPreguntes.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBoxOpcionsFilOr.SuspendLayout();
-            this.groupBoxOrdenacio.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBoxOrdenacio.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPelicules)).BeginInit();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -61,7 +61,7 @@
             // groupBoxPreguntes
             // 
             this.groupBoxPreguntes.Controls.Add(this.groupBoxOpcionsFilOr);
-            this.groupBoxPreguntes.Controls.Add(this.dataGridView1);
+            this.groupBoxPreguntes.Controls.Add(this.dataGridViewPelicules);
             this.groupBoxPreguntes.Controls.Add(this.buttonAcceptarTitul);
             this.groupBoxPreguntes.Controls.Add(this.textBoxTitulo);
             this.groupBoxPreguntes.Controls.Add(this.labelTitul);
@@ -72,39 +72,6 @@
             this.groupBoxPreguntes.TabIndex = 2;
             this.groupBoxPreguntes.TabStop = false;
             this.groupBoxPreguntes.Text = "Preguntes";
-            // 
-            // labelTitul
-            // 
-            this.labelTitul.AutoSize = true;
-            this.labelTitul.Location = new System.Drawing.Point(262, 95);
-            this.labelTitul.Name = "labelTitul";
-            this.labelTitul.Size = new System.Drawing.Size(29, 13);
-            this.labelTitul.TabIndex = 0;
-            this.labelTitul.Text = "Títul";
-            // 
-            // textBoxTitulo
-            // 
-            this.textBoxTitulo.Location = new System.Drawing.Point(332, 92);
-            this.textBoxTitulo.Name = "textBoxTitulo";
-            this.textBoxTitulo.Size = new System.Drawing.Size(394, 20);
-            this.textBoxTitulo.TabIndex = 1;
-            // 
-            // buttonAcceptarTitul
-            // 
-            this.buttonAcceptarTitul.Location = new System.Drawing.Point(749, 90);
-            this.buttonAcceptarTitul.Name = "buttonAcceptarTitul";
-            this.buttonAcceptarTitul.Size = new System.Drawing.Size(75, 23);
-            this.buttonAcceptarTitul.TabIndex = 2;
-            this.buttonAcceptarTitul.Text = "Aceptar";
-            this.buttonAcceptarTitul.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(332, 136);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1472, 835);
-            this.dataGridView1.TabIndex = 3;
             // 
             // groupBoxOpcionsFilOr
             // 
@@ -118,48 +85,6 @@
             this.groupBoxOpcionsFilOr.TabStop = false;
             this.groupBoxOpcionsFilOr.Text = "Opcions";
             // 
-            // labelOrdenacio
-            // 
-            this.labelOrdenacio.AutoSize = true;
-            this.labelOrdenacio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelOrdenacio.Location = new System.Drawing.Point(43, 79);
-            this.labelOrdenacio.Name = "labelOrdenacio";
-            this.labelOrdenacio.Size = new System.Drawing.Size(78, 13);
-            this.labelOrdenacio.TabIndex = 0;
-            this.labelOrdenacio.Text = "Ordenar per:";
-            // 
-            // groupBoxOrdenacio
-            // 
-            this.groupBoxOrdenacio.Controls.Add(this.radioButtonDificultat);
-            this.groupBoxOrdenacio.Controls.Add(this.radioButtonCategoria);
-            this.groupBoxOrdenacio.Location = new System.Drawing.Point(59, 121);
-            this.groupBoxOrdenacio.Name = "groupBoxOrdenacio";
-            this.groupBoxOrdenacio.Size = new System.Drawing.Size(143, 100);
-            this.groupBoxOrdenacio.TabIndex = 1;
-            this.groupBoxOrdenacio.TabStop = false;
-            // 
-            // radioButtonCategoria
-            // 
-            this.radioButtonCategoria.AutoSize = true;
-            this.radioButtonCategoria.Location = new System.Drawing.Point(31, 31);
-            this.radioButtonCategoria.Name = "radioButtonCategoria";
-            this.radioButtonCategoria.Size = new System.Drawing.Size(72, 17);
-            this.radioButtonCategoria.TabIndex = 0;
-            this.radioButtonCategoria.TabStop = true;
-            this.radioButtonCategoria.Text = "Categoría";
-            this.radioButtonCategoria.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonDificultat
-            // 
-            this.radioButtonDificultat.AutoSize = true;
-            this.radioButtonDificultat.Location = new System.Drawing.Point(31, 66);
-            this.radioButtonDificultat.Name = "radioButtonDificultat";
-            this.radioButtonDificultat.Size = new System.Drawing.Size(69, 17);
-            this.radioButtonDificultat.TabIndex = 1;
-            this.radioButtonDificultat.TabStop = true;
-            this.radioButtonDificultat.Text = "Dificultad";
-            this.radioButtonDificultat.UseVisualStyleBackColor = true;
-            // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.buttonEliminar);
@@ -172,14 +97,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
-            // buttonAfegir
+            // buttonEliminar
             // 
-            this.buttonAfegir.Location = new System.Drawing.Point(25, 36);
-            this.buttonAfegir.Name = "buttonAfegir";
-            this.buttonAfegir.Size = new System.Drawing.Size(152, 65);
-            this.buttonAfegir.TabIndex = 0;
-            this.buttonAfegir.Text = "Afegir";
-            this.buttonAfegir.UseVisualStyleBackColor = true;
+            this.buttonEliminar.Location = new System.Drawing.Point(104, 107);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(73, 65);
+            this.buttonEliminar.TabIndex = 2;
+            this.buttonEliminar.Text = "Eliminar";
+            this.buttonEliminar.UseVisualStyleBackColor = true;
             // 
             // buttonEditar
             // 
@@ -190,14 +115,91 @@
             this.buttonEditar.Text = "Editar";
             this.buttonEditar.UseVisualStyleBackColor = true;
             // 
-            // buttonEliminar
+            // buttonAfegir
             // 
-            this.buttonEliminar.Location = new System.Drawing.Point(104, 107);
-            this.buttonEliminar.Name = "buttonEliminar";
-            this.buttonEliminar.Size = new System.Drawing.Size(73, 65);
-            this.buttonEliminar.TabIndex = 2;
-            this.buttonEliminar.Text = "Eliminar";
-            this.buttonEliminar.UseVisualStyleBackColor = true;
+            this.buttonAfegir.Location = new System.Drawing.Point(25, 36);
+            this.buttonAfegir.Name = "buttonAfegir";
+            this.buttonAfegir.Size = new System.Drawing.Size(152, 65);
+            this.buttonAfegir.TabIndex = 0;
+            this.buttonAfegir.Text = "Afegir";
+            this.buttonAfegir.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxOrdenacio
+            // 
+            this.groupBoxOrdenacio.Controls.Add(this.radioButtonDificultat);
+            this.groupBoxOrdenacio.Controls.Add(this.radioButtonCategoria);
+            this.groupBoxOrdenacio.Location = new System.Drawing.Point(59, 121);
+            this.groupBoxOrdenacio.Name = "groupBoxOrdenacio";
+            this.groupBoxOrdenacio.Size = new System.Drawing.Size(143, 100);
+            this.groupBoxOrdenacio.TabIndex = 1;
+            this.groupBoxOrdenacio.TabStop = false;
+            // 
+            // radioButtonDificultat
+            // 
+            this.radioButtonDificultat.AutoSize = true;
+            this.radioButtonDificultat.Location = new System.Drawing.Point(31, 66);
+            this.radioButtonDificultat.Name = "radioButtonDificultat";
+            this.radioButtonDificultat.Size = new System.Drawing.Size(69, 17);
+            this.radioButtonDificultat.TabIndex = 1;
+            this.radioButtonDificultat.TabStop = true;
+            this.radioButtonDificultat.Text = "Dificultad";
+            this.radioButtonDificultat.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCategoria
+            // 
+            this.radioButtonCategoria.AutoSize = true;
+            this.radioButtonCategoria.Location = new System.Drawing.Point(31, 31);
+            this.radioButtonCategoria.Name = "radioButtonCategoria";
+            this.radioButtonCategoria.Size = new System.Drawing.Size(72, 17);
+            this.radioButtonCategoria.TabIndex = 0;
+            this.radioButtonCategoria.TabStop = true;
+            this.radioButtonCategoria.Text = "Categoría";
+            this.radioButtonCategoria.UseVisualStyleBackColor = true;
+            // 
+            // labelOrdenacio
+            // 
+            this.labelOrdenacio.AutoSize = true;
+            this.labelOrdenacio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOrdenacio.Location = new System.Drawing.Point(43, 79);
+            this.labelOrdenacio.Name = "labelOrdenacio";
+            this.labelOrdenacio.Size = new System.Drawing.Size(78, 13);
+            this.labelOrdenacio.TabIndex = 0;
+            this.labelOrdenacio.Text = "Ordenar per:";
+            // 
+            // dataGridViewPelicules
+            // 
+            this.dataGridViewPelicules.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.dataGridViewPelicules.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridViewPelicules.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPelicules.Location = new System.Drawing.Point(332, 136);
+            this.dataGridViewPelicules.Name = "dataGridViewPelicules";
+            this.dataGridViewPelicules.Size = new System.Drawing.Size(1472, 835);
+            this.dataGridViewPelicules.TabIndex = 3;
+            // 
+            // buttonAcceptarTitul
+            // 
+            this.buttonAcceptarTitul.Location = new System.Drawing.Point(749, 90);
+            this.buttonAcceptarTitul.Name = "buttonAcceptarTitul";
+            this.buttonAcceptarTitul.Size = new System.Drawing.Size(75, 23);
+            this.buttonAcceptarTitul.TabIndex = 2;
+            this.buttonAcceptarTitul.Text = "Aceptar";
+            this.buttonAcceptarTitul.UseVisualStyleBackColor = true;
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.Location = new System.Drawing.Point(332, 92);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(394, 20);
+            this.textBoxTitulo.TabIndex = 1;
+            // 
+            // labelTitul
+            // 
+            this.labelTitul.AutoSize = true;
+            this.labelTitul.Location = new System.Drawing.Point(267, 95);
+            this.labelTitul.Name = "labelTitul";
+            this.labelTitul.Size = new System.Drawing.Size(29, 13);
+            this.labelTitul.TabIndex = 0;
+            this.labelTitul.Text = "Títul";
             // 
             // FormPreguntas
             // 
@@ -208,14 +210,15 @@
             this.Controls.Add(this.toolStrip1);
             this.Name = "FormPreguntas";
             this.Text = "FormPreguntas";
+            this.Load += new System.EventHandler(this.FormPreguntas_Load);
             this.groupBoxPreguntes.ResumeLayout(false);
             this.groupBoxPreguntes.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBoxOpcionsFilOr.ResumeLayout(false);
             this.groupBoxOpcionsFilOr.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
             this.groupBoxOrdenacio.ResumeLayout(false);
             this.groupBoxOrdenacio.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPelicules)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,7 +228,7 @@
 
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.GroupBox groupBoxPreguntes;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridViewPelicules;
         private System.Windows.Forms.Button buttonAcceptarTitul;
         private System.Windows.Forms.TextBox textBoxTitulo;
         private System.Windows.Forms.Label labelTitul;
